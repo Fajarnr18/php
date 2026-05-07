a7828a0fe357764384a6894b5d23034348b4d2b8
Fajarnr18 <fajarnurramadhan2@gmail.com>
2026-05-07 11:58:49 +0700

    Change page title from 'Modern CRUD Users' to 'Tambah Data'

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ if (isset($_POST['update'])) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Modern CRUD Users</title>
+<title>Tambah Data</title>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
